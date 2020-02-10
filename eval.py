c88c0e807adbcc3a3816cacb97b38d41e2f0dc30
_HYX_ <2757567940@qq.com>
2020-02-10 18:44:42 +0800

    Add files via upload

---
+++ eval.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from models import vgg
+import torch.optim as optim
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='train VGGnet in CIFAR10 dataset')
+parser.add_argument('--deform', '-d', type=int, default=0,
+                    help='Whether to replace ordinary convolution with deformable convolution(default:True)')
+args = parser.parse_args()
+
+deform = True if args.defrom > 0 else False
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+net = vgg.vgg16(deformable=deform, num_classes=10)
+PATH = './cifar_net2.pth' if deform else './cifar_net.pth'
+net.load_state_dict(torch.load(PATH))
+
+net.to(device)
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].to(device), data[1].to(device)
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].to(device), data[1].to(device)
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
---
+++ train.py
@@ -0,0 +1,85 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from models import vgg
+import torch.optim as optim
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='train VGGnet in CIFAR10 dataset')
+parser.add_argument('--deform', '-d', type=int, default=0,
+                    help='Whether to replace ordinary convolution with deformable convolution(default:True)')
+args = parser.parse_args()
+
+deform = True if args.defrom > 0 else False
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=False, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=False, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+net = vgg.vgg16(deformable=deform, num_classes=10)
+net.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        if i % 10 == 0:
+            print('i: ', i)
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data[0].to(device), data[1].to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+        # break
+
+print('Finished Training')
+
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].to(device), data[1].to(device)
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        # break
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
